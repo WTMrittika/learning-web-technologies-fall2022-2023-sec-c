e56cd36a70308d78c460c637a5aa10080c078a9a
Mrittika, Wasifa Tasnim <131018925+WTMrittika@users.noreply.github.com>
2023-10-21 11:56:50 +0600

    registration.php

--- lab4_validation/registration.php
+++ lab4_validation/registration.php
@@ -10,12 +10,10 @@ if (isset($_POST['submit'])) {
     $gender = $_POST['gender'];
     $dob = $_POST['dob'];
 
-    // Validation rules for username
     if (strlen($username) < 2 || !isValidUsername($username)) {
         $error_messages[] = "Username is invalid. It must contain at least two alphanumeric characters, period, dash, or underscore only.";
     }
 
-    // Validation rules for password
     if (strlen($password) < 8 || !isValidPassword($password)) {
         $error_messages[] = "Password is invalid. It must not be less than eight characters and contain at least one of the special characters (@, #, $, %).";
     }
@@ -25,12 +23,8 @@ if (isset($_POST['submit'])) {
     }
 
     if (empty($error_messages)) {
-        // Validation passed, and no errors
-        // You can save the registration data here
-        // For simplicity, we'll just display a success message
         echo "Registration successful!";
     } else {
-        // Display error messages in black color
         foreach ($error_messages as $error_message) {
             echo '<font color="black">' . $error_message . '</font><br>';
         }
