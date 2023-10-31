8fee712c6e4e84bbda48fcd599f96f49311af673
WTMrittika <21-45108-2@student.aiub.edu>
2023-10-31 17:21:24 +0600

    login registration contoller

---
+++ MID_LAB_EXAM/model/db.php
@@ -0,0 +1,19 @@
+<?php 
+
+$dbhost = 'localhost';
+$dbname = 'mid';
+$dbuser = 'root';
+$dbpassword = '';
+
+
+function getConnection(){
+    global $dbhost;
+    global $dbname;
+    global $dbpassword;
+    global $dbuser;
+
+    $con = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+    return $con;
+}
+
+?>
